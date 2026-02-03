c29a179ab5bf77e8f4200cdc1c46c4ae9e93da5e
Nishant Chakraborty <ncnishant333@gmail.com>
2026-02-03 12:45:10 +0530

    connected to database

--- src/app.js
+++ src/app.js
@@ -2,7 +2,18 @@ const express = require ('express')
 
 const app = express()
 
+const connectionDb = require('./configuration/connection')
+
 const {userAuth,hrAuth}= require('./utils/middleware')
+
+connectionDb().then(()=>{
+    console.log('connected to database')
+    app.listen(5000,()=>{
+    console.log('server is listening')
+})
+}).catch((err)=>{
+    console.log(err)
+})
 
 app.use('/user',userAuth)
 
@@ -22,6 +33,3 @@ app.get('/hr/profile',hrAuth, (req, res)=>{
     res.send('profile for hr1')
 })
 
-app.listen(5000,()=>{
-    console.log('server is listening')
-})
